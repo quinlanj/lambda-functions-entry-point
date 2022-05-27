36a65e3f4bd13684c3505bc00100220128273990
Quinlan Jung <quin@expo.io>
2022-05-27 00:57:54 -0700

    build stuff

--- dist/app.js
+++ dist/app.js
@@ -2,83 +2,28 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const tslib_1 = require("tslib");
-const cors_1 = tslib_1.__importDefault(require("cors"));
 const express_1 = tslib_1.__importDefault(require("express"));
+const fs_1 = tslib_1.__importDefault(require("fs"));
 const path_1 = tslib_1.__importDefault(require("path"));
-const compression_1 = tslib_1.__importDefault(require("compression"));
-const serverless_express_1 = require("@vendia/serverless-express");
-const ejs = require("ejs").__express;
 const app = (0, express_1.default)();
 exports.app = app;
-const router = express_1.default.Router();
-app.set("view engine", "ejs");
-app.engine(".ejs", ejs);
-router.use((0, compression_1.default)());
-router.use((0, cors_1.default)());
-router.use(express_1.default.json());
-router.use(express_1.default.urlencoded({ extended: true }));
-app.set("views", path_1.default.join(__dirname, "views"));
-router.get("/", (req, res) => {
-    const currentInvoke = (0, serverless_express_1.getCurrentInvoke)();
-    const { event = {} } = currentInvoke;
-    const { requestContext = {} } = event;
-    const { domainName = "localhost:3000" } = requestContext;
-    const apiUrl = `https://${domainName}`;
-    return res.render("index", {
-        apiUrl,
-    });
-});
-router.get("/vendia", (req, res) => {
-    return res.sendFile(path_1.default.join(__dirname, "vendia-logo.png"));
-});
-router.get("/users", (req, res) => {
-    return res.json(users);
-});
-router.get("/users/:userId", (req, res) => {
-    const user = getUser(req.params.userId);
-    if (!user)
-        return res.status(404).json({});
-    return res.json(user);
-});
-router.post("/users", (req, res) => {
-    const user = {
-        id: ++userIdCounter,
-        name: req.body.name,
-    };
-    users.push(user);
-    return res.status(201).json(user);
-});
-router.put("/users/:userId", (req, res) => {
-    const user = getUser(req.params.userId);
-    if (!user)
-        return res.status(404).json({});
-    user.name = req.body.name;
-    return res.json(user);
-});
-router.delete("/users/:userId", (req, res) => {
-    const userIndex = getUserIndex(req.params.userId);
-    if (userIndex === -1)
-        return res.status(404).json({});
-    users.splice(userIndex, 1);
-    return res.json(users);
-});
-router.get("/cookie", (req, res) => {
-    res.cookie("Foo", "bar");
-    res.cookie("Fizz", "buzz");
-    return res.json({});
-});
-const getUser = (userId) => users.find((u) => u.id === parseInt(userId));
-const getUserIndex = (userId) => users.findIndex((u) => u.id === parseInt(userId));
-const users = [
-    {
-        id: 1,
-        name: "Joe",
-    },
-    {
-        id: 2,
-        name: "Jane",
-    },
-];
-let userIdCounter = users.length;
-app.use("/", router);
+app.use('/', express_1.default.static('web-build'));
+const FUNCTIONS_DIRNAME = 'functions';
+const files = fs_1.default.readdirSync(FUNCTIONS_DIRNAME);
+for (const file of files) {
+    const filenameNoExtension = path_1.default.parse(file).name;
+    if (filenameNoExtension === '.gitignore') {
+        continue;
+    }
+    const relativePathToIndex = path_1.default.join(FUNCTIONS_DIRNAME, filenameNoExtension);
+    try {
+        const userDefinedModule = require(relativePathToIndex);
+        const mountedPath = path_1.default.join('/', FUNCTIONS_DIRNAME, filenameNoExtension).toString();
+        app.use(mountedPath, userDefinedModule.default);
+        console.log('Function mounted at:', mountedPath);
+    }
+    catch (e) {
+        console.error(`Error mounting function ${filenameNoExtension}`, e);
+    }
+}
 //# sourceMappingURL=app.js.map
